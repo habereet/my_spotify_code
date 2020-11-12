00e0449a429c7801d54475d0a39c2f6d9f840705
habereet <habereet@gmail.com>
2020-11-11 20:52:33 -0500

    Move Variables class to separate file

--- spotify.py
+++ spotify.py
@@ -1,57 +1,15 @@
 import sys
-import os
+from environment import Variables
 from dotenv import load_dotenv
 import spotipy
 import spotipy.util as util
 
 
-# Variables class will store API secrets from stored in .env
-# TODO implement try/catch in case .env isn't set up correctly
-class Variables:
-    def __init__(self, commandLineArgs):
-        load_dotenv()
-        self.set_clientID(os.getenv("client_id"))
-        self.set_clientSecret(os.getenv("client_secret"))
-        self.set_redirectURL(os.getenv("redirect_url"))
-        self.set_username(commandLineArgs)
-        self.set_token()
-
-    def set_username(self, commandLineArgs):
-        try:
-            username = os.getenv("username")
-            if username is None:
-                raise ValueError()
-            else:
-                self.username = username
-        except ValueError:
-            if len(commandLineArgs) > 1:
-                self.username = commandLineArgs[1]
-            else:
-                print("Username not provided")
-                sys.exit()
-
-    def set_clientID(self, clientID):
-        self.clientID = clientID
-
-    def set_clientSecret(self, clientSecret):
-        self.clientSecret = clientSecret
-
-    def set_redirectURL(self, redirectURL):
-        self.redirectURL = redirectURL
-
-    def set_token(self):
-        scope = 'user-library-read'
-        self.token = util.prompt_for_user_token(self.username,
-                                                scope,
-                                                self.clientID,
-                                                self.clientSecret,
-                                                self.redirectURL)
-
 
 # TODO explain search
 # TODO implement search
 def search(spotipyToken, query, target_market="US"):
-    searchResults = spotipyToken.search(q=query, market=target_market)
+    searchResults = spotipyToken.search(q=query, market=target_market) 
     print(searchResults)
 
 
